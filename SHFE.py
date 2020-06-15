638671b1a5e61269cdf587c3471595af30068986
dongyuchi <542264324@qq.com>
2020-06-15 09:02:42 +0800

    初始化

--- SHFE.py
+++ SHFE.py
@@ -3,12 +3,20 @@
 上海期货交易所
 
 """
+from selenium import webdriver
 
 # IEdriver(忽略安全提示)
-def IEDdriver():
-    pass
+def IEbrowserdriver():
+    capabilities = webdriver.DesiredCapabilities().INTERNETEXPLORER
+    capabilities['acceptSslCerts'] = True
+    driver = webdriver.Ie(capabilities=capabilities)
+    return driver
 
 
 # 登录
 def login():
     pass
+
+
+if __name__ == "__main__":
+    driver = IEbrowserdriver()
